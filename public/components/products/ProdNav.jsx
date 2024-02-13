be9063394393b612e96b13542466b80c13973970
FaridRuano <fruanocm2777@gmail.com>
2024-02-13 17:44:30 -0500

    Bug Fix

--- public/components/products/ProdNav.jsx
+++ public/components/products/ProdNav.jsx
@@ -1,17 +1,56 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation";
 
 const ProdNav = () => {
   const router = useRouter()
 
-  const [active, setActive] = useState(1)
+  const [active, setActive] = useState(parseInt(localStorage.getItem("activeMenu")) || 1)
 
   const activateNav = (menu, link) => {
     setActive(menu)
+    localStorage.setItem("activeMenu", menu.toString())
     router.push(link)
-}
+  }
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      const currentPath = router.asPath;
+      if (currentPath.startsWith("/productos/")) {
+        switch (currentPath) {
+          case "/productos/":
+            setActive(1);
+            break;
+          case "/productos/credito":
+            setActive(2);
+            break;
+          case "/productos/inversiones":
+            setActive(3);
+            break;
+          case "/productos/tarjetas":
+            setActive(4);
+            break;
+          default:
+            setActive(1);
+            break;
+        }
+      }
+    };
+  
+    // Check if router.events is defined before using it
+    if (router && router.events) {
+      router.events.on("routeChangeComplete", handleRouteChange);
+  
+      // Set initial active state
+      handleRouteChange();
+  
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
+  }, [router]);
+  
 
   return (
     <>
