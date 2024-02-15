f4a88041b041554df420ada1b273bd31339dd33b
FaridRuano <fruanocm2777@gmail.com>
2024-02-15 14:29:14 -0500

    Tec Form

--- app/(pages)/productos/tarjetas/page.jsx
+++ app/(pages)/productos/tarjetas/page.jsx
@@ -1,7 +1,46 @@
+'use client'
+import Image from "next/image"
+import FrSdCard from "@public/images/frside-card.png"
+import { useEffect } from "react"
 
 const Cards = () => {
+
+  useEffect(()=>{
+    window.addEventListener('scroll', function () {
+      let card = document.getElementById('card-3d')
+
+      let scrollTop = window.scrollY
+      console.log(scrollTop)
+
+      let fadeOut = 458
+      let cardLimit = 818
+      let cardGone = 1276
+      
+      if(scrollTop > cardLimit){
+        card.classList.add("zoom")
+        if(scrollTop > cardGone){
+          card.style.opacity = 0
+        }else{
+          card.style.opacity = 1 - Math.min((scrollTop - 818) / fadeOut, 1)
+        }
+        card.classList.add("default")
+      }else{
+        card.classList.remove("zoom")
+      }
+      
+    })
+  },[])
   return (
-    <div>Cards</div>
+    <>
+      <section className="main-ta">
+        <Image id="card-3d" className="card-frsd" src={FrSdCard} width={800} height={'auto'}/>
+        {/* <Image  className="card-frsd zoom" src={FrSdCard} width={800} height={'auto'}/> */}
+
+      </section>
+      <section className="second-ta">
+
+      </section>
+    </>
   )
 }
 
---
+++ app/(pages)/tecform/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TecForm = () => {
+  return (
+    <div>TecForm</div>
+  )
+}
+
+export default TecForm
