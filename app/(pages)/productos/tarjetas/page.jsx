fcffa050bac32a11fe4de5b999bca9bc399b903a
FaridRuano <fruanocm2777@gmail.com>
2024-03-14 18:11:23 -0500

    Card Style

--- app/(pages)/productos/tarjetas/page.jsx
+++ app/(pages)/productos/tarjetas/page.jsx
@@ -8,10 +8,14 @@ const Cards = () => {
   useEffect(()=>{
     window.addEventListener('scroll', function () {
       let card = document.getElementById('card-3d')
+      let info1 = document.getElementById('info-1')
+
+      
 
       let scrollTop = window.scrollY
       console.log(scrollTop)
-
+      
+      /* Card */
       let fadeOut = 458
       let cardLimit = 818
       let cardGone = 1276
@@ -21,24 +25,28 @@ const Cards = () => {
         if(scrollTop > cardGone){
           card.style.opacity = 0
         }else{
-          if(scrollTop < cardLimit){
-            card.style.opacity = 1
-          }else{
-          }
+          card.style.opacity = 1 - Math.min((scrollTop - 817) / fadeOut, 1)
         }
         card.classList.add("default")
       }else{
+        card.style.opacity = 1
         card.classList.remove("zoom")
       }
-      
+
+      /* Main Info */
+      if(scrollTop > 300){
+        info1.style.opacity=0
+      }else{
+        info1.style.opacity=1
+      }
     })
   },[])
   return (
     <>
       <section className="main-ta">
-        <Image id="card-3d" className="card-frsd" src={FrSdCard} width={800} height={'auto'}/>
+        <Image id="card-3d" className="card-frsd" src={FrSdCard} width={800} height={'auto'} alt="Sanfra Debit"/>
         {/* <Image  className="card-frsd zoom" src={FrSdCard} width={800} height={'auto'}/> */}
-        <div className="main-info">
+        <div className="main-info" id="info-1">
           <div className="intro">
             <span>
               Solícita <b>NUEVA</b> Sanfra
